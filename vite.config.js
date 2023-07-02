b669d77c9e3f0a966612fabd93a46dfe0c6059d7
RayyanNafees <nafees.rayyan@gmail.com>
2023-07-02 18:18:09 +0530

    Finally resolved vite config errors

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   
   resolve: {
     alias: {
-      '@/': path.resolve(__dirname, 'leadmanager/frontend/src') + '/',
+      '@': path.resolve(__dirname, 'leadmanager/frontend/src')// + '/',
     },
   },
   
